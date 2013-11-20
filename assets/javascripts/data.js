8f0e6241e2ed50ab3fe480072966592783a7d8db
Guilherme Simoes <guilherme.rdems@gmail.com>
2013-11-20 23:22:33 +0000

    Project start.
    For now, the data being used is from Game of Thrones and it is hardcoded.
    The plan is to make the whole chart dynamic.

---
+++ assets/javascripts/data.js
@@ -0,0 +1,182 @@
+var episodes = [
+  {
+    id: "S1E1",
+    rating: "8.7",
+    name: "Winter Is Coming",
+    url: "/title/tt1480055/"
+  },
+  {
+    id: "S1E2",
+    rating: "8.6",
+    name: "The Kingsroad",
+    url: "/title/tt1668746/"
+  },
+  {
+    id: "S1E3",
+    rating: "8.5",
+    name: "Lord Snow",
+    url: "/title/tt1829962/"
+  },
+  {
+    id: "S1E4",
+    rating: "8.5",
+    name: "Cripples, Bastards, and Broken Things",
+    url: "/title/tt1829963/"
+  },
+  {
+    id: "S1E5",
+    rating: "8.8",
+    name: "The Wolf and the Lion",
+    url: "/title/tt1829964/"
+  },
+  {
+    id: "S1E6",
+    rating: "8.8",
+    name: "A Golden Crown",
+    url: "/title/tt1837862/"
+  },
+  {
+    id: "S1E7",
+    rating: "8.9",
+    name: "You Win or You Die",
+    url: "/title/tt1837863/"
+  },
+  {
+    id: "S1E8",
+    rating: "8.8",
+    name: "The Pointy End",
+    url: "/title/tt1837864/"
+  },
+  {
+    id: "S1E9",
+    rating: "9.2",
+    name: "Baelor",
+    url: "/title/tt1851398/"
+  },
+  {
+    id: "S1E10",
+    rating: "9.1",
+    name: "Fire and Blood",
+    url: "/title/tt1851397/"
+  },
+  {
+    id: "S2E1",
+    rating: "8.6",
+    name: "The North Remembers",
+    url: "/title/tt1971833/"
+  },
+  {
+    id: "S2E2",
+    rating: "8.3",
+    name: "The Night Lands",
+    url: "/title/tt2069318/"
+  },
+  {
+    id: "S2E3",
+    rating: "8.7",
+    name: "What Is Dead May Never Die",
+    url: "/title/tt2070135/"
+  },
+  {
+    id: "S2E4",
+    rating: "8.6",
+    name: "Garden of Bones",
+    url: "/title/tt2069319/"
+  },
+  {
+    id: "S2E5",
+    rating: "8.6",
+    name: "The Ghost of Harrenhal",
+    url: "/title/tt2074658/"
+  },
+  {
+    id: "S2E6",
+    rating: "8.7",
+    name: "The Old Gods and the New",
+    url: "/title/tt2085238/"
+  },
+  {
+    id: "S2E7",
+    rating: "8.7",
+    name: "A Man Without Honor",
+    url: "/title/tt2085239/"
+  },
+  {
+    id: "S2E8",
+    rating: "8.6",
+    name: "The Prince of Winterfell",
+    url: "/title/tt2085240/"
+  },
+  {
+    id: "S2E9",
+    rating: "9.4",
+    name: "Blackwater",
+    url: "/title/tt2084342/"
+  },
+  {
+    id: "S2E10",
+    rating: "9.0",
+    name: "Valar Morghulis",
+    url: "/title/tt2112510/"
+  },
+  {
+    id: "S3E1",
+    rating: "8.6",
+    name: "Valar Dohaeris",
+    url: "/title/tt2178782/"
+  },
+  {
+    id: "S3E2",
+    rating: "8.4",
+    name: "Dark Wings, Dark Words",
+    url: "/title/tt2178772/"
+  },
+  {
+    id: "S3E3",
+    rating: "8.6",
+    name: "Walk of Punishment",
+    url: "/title/tt2178802/"
+  },
+  {
+    id: "S3E4",
+    rating: "9.3",
+    name: "And Now His Watch Is Ended",
+    url: "/title/tt2178798/"
+  },
+  {
+    id: "S3E5",
+    rating: "8.8",
+    name: "Kissed by Fire",
+    url: "/title/tt2178788/"
+  },
+  {
+    id: "S3E6",
+    rating: "8.5",
+    name: "The Climb",
+    url: "/title/tt2178812/"
+  },
+  {
+    id: "S3E7",
+    rating: "8.4",
+    name: "The Bear and the Maiden Fair",
+    url: "/title/tt2178814/"
+  },
+  {
+    id: "S3E8",
+    rating: "8.8",
+    name: "Second Sons",
+    url: "/title/tt2178806/"
+  },
+  {
+    id: "S3E9",
+    rating: "9.7",
+    name: "The Rains of Castamere",
+    url: "/title/tt2178784/"
+  },
+  {
+    id: "S3E10",
+    rating: "8.9",
+    name: "Mhysa",
+    url: "/title/tt2178796/"
+  }
+]
---
+++ assets/javascripts/main.js
@@ -0,0 +1,62 @@
+var heightMargin = 20, /* This is needed because of browsers' margins and padding and whatnot */
+    width = document.body.offsetWidth,
+    height = window.innerHeight - heightMargin,
+    yAxisWidth = 30,
+    xAxisHeight = 30,
+    chartBottomPadding = 10,
+    chartWidth = width - yAxisWidth,
+    chartHeight = height - chartBottomPadding - xAxisHeight,
+    linePadding = 1; /* https://github.com/mbostock/d3/wiki/Ordinal-Scales#wiki-ordinal_rangePoints */
+
+// scales
+var x = d3.scale.ordinal()
+    .rangePoints([0, chartWidth], linePadding);
+var y = d3.scale.linear()
+    .range([chartHeight, 0]);
+
+// axis
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var xPos = function(d) { return x(d.id); };
+var yPos = function(d) { return y(d.rating); };
+
+ids = episodes.map(function(d){ return d.id; });
+x.domain(ids);
+y.domain(d3.extent(episodes, function(d) { return d.rating; }));
+
+var line = d3.svg.line()
+    .x(xPos)
+    .y(yPos);
+
+var svg = d3.select("body").append("svg")
+    .attr("width", width)
+    .attr("height", height);
+
+var lineContainer = svg.append("g")
+    .attr("transform", "translate(" + yAxisWidth + ",0)");
+
+svg.append("g")
+    .attr("class", "x axis")
+    .attr("transform", "translate(" + yAxisWidth + "," + (chartHeight + chartBottomPadding) + ")")
+    .call(xAxis);
+
+svg.append("g")
+    .attr("class", "y axis")
+    .attr("transform", "translate(" + yAxisWidth + ",0)")
+    .call(yAxis)
+    .append("text")
+        .attr("transform", "rotate(-90)")
+        .attr("y", 6)
+        .attr("dy", ".71em")
+        .style("text-anchor", "end")
+        .text("Average Rating");
+
+lineContainer.append("path")
+    .datum(episodes)
+    .attr("class", "line")
+    .attr("d", line);
